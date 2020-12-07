a37253ca6167caf3cb0db213509762e96f74ac48
samar <Sa1617778sa@gmail.com>
2020-12-07 13:34:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import img from './imageInSrc.jpg';
+
+function App() {
+    return (
+        
+        <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+        <h1 className="title red">Your name here</h1>
+       
+        <br />
+       
+        <img src={img} alt="bag"/>
+       
+        <br />
+       
+        <img src="/imageInPublic.jpg" alt="shooes"/>
+       
+      
+       
+       <video width="320" height="240" controls>
+       
+        <source src="myVideo.mp4" type="video/mp4" />
+       
+       </video>  
+       </div> 
+        
+    );
+}
+
+export default App;
